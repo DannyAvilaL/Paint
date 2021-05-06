dab289cecb834b738201518c61aa87d95a9f6f57
DannyAvilaL <a01378664@itesm.mx>
2021-05-06 14:30:35 -0500

    Documentación corregida.

--- paint.py
+++ paint.py
@@ -1,7 +1,3 @@
-#Autores
-# Daniela Avila Luna    A01378664
-# Liam Garay Monroy     A01750632
-
 """Paint, for drawing shapes.
 
 Exercises
@@ -13,83 +9,101 @@ Exercises
 5. Add width parameter.
 
 """
-#Importa las librerías a utilizar
+
+# Código modificado
+# Autor: Daniela Avila Luna    A01378664
+# Autor: Liam Garay Monroy     A01750632
+
+# Importa las librerías a utilizar
 from turtle import *
 from freegames import vector
 from math import tan, hypot
 
 def line(start, end):
-    "Define el funcionamiento de la línea"
-    up()        #Levanta la pluma de forma que aún no dibuja
-    goto(start.x, start.y)      #se dan las coordenas iiciales de la línea
-    down()      #baja la pluma para comenzar a dibujar
-    goto(end.x, end.y)      #Lleva la pluma a las segundas coordenadas dibujando la línea      
+    """Define el funcionamiento de la línea"""
+    up()        
+    goto(start.x, start.y)      
+    down()      
+    goto(end.x, end.y)     
+
 
 def square(start, end):
-    "Define el comportamiento de el cuadrado"
-    up()        #Levanta la pluma de forma que aún no dibuja
-    goto(start.x, start.y)      #se dan las coordenas iniciales de la primera esquina del cuadrado
-    down()      #baja la pluma para comenzar a dibujar
-    begin_fill()        #Se llama a esta función antes de hacer una figura para que la misma se llene        
+    """Define el comportamiento de el cuadrado"""
+    up()       
+    goto(start.x, start.y)      
+    down()
+    begin_fill()
 
-    for count in range(4):      #se realiza un for que trazara las cuatro líneas del cuadrado     
-        forward(end.x - start.x)        #se avanza la distancia que hay entre el punto de inicio y el punto del final
-        left(90)        #se gira 90 grados cada vez que se entra al for, con el fin de que las 4 lineas formen un cuadrado
+    # se realiza un for que trazara las cuatro líneas del cuadrado 
+    for count in range(4):          
+        forward(end.x - start.x)        
+        left(90)        
 
-    end_fill()      #Se termina el llenado de la figura 
+    end_fill()      
+
 
 def draw_circle(start, end):
-    "Dibuja un circulo obteniendo el punto de inicio y el final del mismo utilizándolos como diametro"
+    """Dibuja un circulo obteniendo el punto de inicio y 
+    el final del mismo utilizándolos como diametro"""
     lado = round(hypot(end.x - start.x, end.y - start.y))
-    radio = lado/2     #Se obtiene la distancia entre el principio y el final del circulo y se divide entre 2 para obtener su radio
-    up()        #Levanta la pluma de forma que aún no dibuja
-    goto(start.x, start.y)      #se dan las coordenas iniciales del circulo
-    down()      #baja la pluma para comenzar a dibujar
-    seth(towards(end.x, end.y)-90)     #apunta en la dirección del segundo punto, restando 90° para coincidir con los puntos
-    begin_fill()        #Se llama a esta función antes de hacer una figura para que la misma se llene
-    circle(radio)       #se llama a esta función con el radio del círculo, lo que permitirá que se dibuje
-    end_fill()      #Se termina el llenado de la figura
+    radio = lado/2   
+    up()      
+    goto(start.x, start.y)      
+    down()      
+    seth(towards(end.x, end.y)-90)     
+    begin_fill()        
+    circle(radio)       
+    end_fill()      
+
 
 def rectangle(start, end):
-    "Dibuja un rectángulo al dar las coordenadas de inicio y fin con el mouse."
-    up()        #Levanta la pluma
-    goto(start.x, start.y)      #Va a las coordenadas de inicio
-    down()      #Baja la pluma
-    begin_fill()        #Comienza a colorear
-    seth(0)     # Apunta hacia la derecha para poder trazar correctamente el rectángulo
-    for lado in range(2):       #Trazará medio rectángulo por iteración
+    """Dibuja un rectángulo al dar las coordenadas
+    de inicio y fin con el mouse."""
+    up()       
+    goto(start.x, start.y)     
+    down()     
+    begin_fill()        
+    seth(0)    
+
+    for lado in range(2):       # Trazará medio rectángulo por iteración
         forward(end.x - start.x)    
         left(90)
-        forward(end.y - start.y)        #Aquí llega al segundo punto indicado
+        forward(end.y - start.y)       
         left(90)
 
-    end_fill()      #Termina de colorear
+    end_fill()     
+
 
 def triangle(start, end):
-    "Dibuja un triángulo equilátero al dar las coordenadas de inicio y fin de un lado."
-    up()        #Sube lápiz
-    goto(start.x, start.y)      #Va a las coordenadas de inicio
-    down()      #Baja lápiz
-    begin_fill()    #Comienza a colorear
-    lado = round(hypot(end.x - start.x, end.y - start.y))   #Mide la longitud del lado con la hipotenusa de un triángulo rectángulo
-    seth(towards(end.x, end.y))     #Apunta en la dirección del segundo punto
-    for lados in range(3):      #Realiza un triángulo equilátero al girar 120
+    """Dibuja un triángulo equilátero al dar las coordenadas
+    de inicio y fin de un lado."""
+    up()      
+    goto(start.x, start.y)     
+    down()      
+    begin_fill()
+    # Mide la longitud del lado con la hipotenusa de un triángulo rectángulo
+    lado = round(hypot(end.x - start.x, end.y - start.y))   
+    seth(towards(end.x, end.y))     # Apunta en la dirección del segundo punto
+
+    for lados in range(3):      # Realiza un triángulo equilátero al girar 120
         forward(lado)
         left(120)
     
-    end_fill()      #Termina de colorear
+    end_fill()      # Termina de colorear
         
-def tap(x, y):
-    "Almacena las coordenadas cada vez que se de un click en la pantalla"
-    start = state['start']      #obtiene el estado actual de state, tipo diccionario
 
-    if start is None:       # Si es None, entonces almacena el valor de las coordenadas de inicio
+def tap(x, y):
+    """Almacena las coordenadas cada vez que se de un click en la pantalla"""
+    start = state['start']     
+
+    if start is None:       
         state['start'] = vector(x, y)
-    else:                   # Si no es None, entonces obtiene la figura a trazar para llamar a esa función
+    else:                 
         shape = state['shape']
-        end = vector(x, y)      # Almacena las coordenadas del punto de fin
-        shape(start, end)       # Se envían las coordenadas de incio y final a la función actualmente seleccionada
-        state['start'] = None   # Después de realizar el trazo, actualiza el estado de incio a None.
+        end = vector(x, y)      
+        shape(start, end)      
+        state['start'] = None   
+
 
 def store(key, value):
     """
@@ -98,20 +112,26 @@ def store(key, value):
     """
     state[key] = value
 
-state = {'start': None, 'shape': line}      # Al momento de inciar el programa, guardará el primer punto y el trazo por default es una línea
-setup(420, 420, 370, 0)     # Dimensiones de la ventana
-onscreenclick(tap)      # Con cada click, llamará a la función de tap
-listen()        #Función que ayudará a tener actualizado la detección de eventos en la ventana.
-onkey(undo, 'u')        # Regresa un paso al trazo de la tortuga
-onkey(lambda: color('black'), 'K')      # Cambia a color negro con K
-onkey(lambda: color('white'), 'W')      # Cambia al color blanco con W
-onkey(lambda: color('green'), 'G')      # Cambia al color verde con G
-onkey(lambda: color('blue'), 'B')       # Cambia al color azul con B
-onkey(lambda: color('red'), 'R')        # Cambia al color rojo con R
-onkey(lambda: color('yellow'), 'Y')        # Cambia al color amarillo con Y
-onkey(lambda: store('shape', line), 'l')        # Pinta una línea recta con l
-onkey(lambda: store('shape', square), 's')      # Pinta un cuadrado con s
-onkey(lambda: store('shape', draw_circle), 'c')      # Pinta un círculo con c
-onkey(lambda: store('shape', rectangle), 'r')   # Pinta un rectángulo con r
-onkey(lambda: store('shape', triangle), 't')    # Pinta un triángulo con t
+
+state = {'start': None, 'shape': line}     
+setup(420, 420, 370, 0)    
+onscreenclick(tap)      
+listen()       
+onkey(undo, 'u')        
+
+# Cambia de color dependiendo la tecla presionada
+onkey(lambda: color('black'), 'K')      
+onkey(lambda: color('white'), 'W')      
+onkey(lambda: color('green'), 'G')     
+onkey(lambda: color('blue'), 'B')      
+onkey(lambda: color('red'), 'R')      
+onkey(lambda: color('yellow'), 'Y')     
+
+# Cambia de dirección dependiendo la tecla presionada
+onkey(lambda: store('shape', line), 'l')        
+onkey(lambda: store('shape', square), 's')      
+onkey(lambda: store('shape', draw_circle), 'c')     
+onkey(lambda: store('shape', rectangle), 'r')   
+onkey(lambda: store('shape', triangle), 't')    
+
 done()
